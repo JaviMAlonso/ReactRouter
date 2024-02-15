317672887e0570cb54ba222217690df4c326ead7
JaviMAlonso <rodrijavi3002@gmail.com>
2024-02-15 10:39:52 +0100

    cosoError

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
